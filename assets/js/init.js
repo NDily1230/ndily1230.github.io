d1f0e5b2614d5e8f2f37f582a825c938d6a56e94
mimi <ndily1230@gmail.com>
2017-05-18 14:32:43 -0400

    materialize rewrite

---
+++ assets/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+		closeOnClick: true,
+		draggable: true
+  		}); // end of document ready
+	})(jQuery); // end of jQuery name space
