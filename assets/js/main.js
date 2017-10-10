a1d7b378656f2a70d1d151610b2ac4aeca569329
mimi <ndily1230@gmail.com>
2017-10-10 10:37:28 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,38 +11,12 @@ $(".nav-item").on('click', function(e) {
         scrollTop: $(section).offset().top
     });
 });
-
 $('#btn').on('click', function(e){
 	e.preventDefault();
 	console.log("will scroll");
     $("html, body").animate({
         scrollTop: $("#about").offset().top
     });
+});
 
-    $(".contact").on("click", function(e) {
-        $(".contact-us").validate({
-            submitHandler: function() {
-                var form_data = new FormData(this.currentForm); //constructs key/value pairs representing fields and values  
-                for (var pair of form_data.entries()) {
-                    console.log(pair[0]+ ', ' + pair[1]); 
-                };  
-            $.ajax({
-                url: "../assets/ajax/mailer.php",
-                type: "POST",
-                data : form_data,
-                contentType: false,
-                cache: false,
-                processData:false,
-                }).done(function(res){ //fetch server "json" messages when done
-                    console.log(res);
-                    if (res) {
-                        $(".messageSent").show(); 
-                    }
-                    else {
-                        $(".messageError").show();
-                    }
-            });
-            }
-        });
-    });    
-})
+$('.carousel').carousel();
